497ff2d84de3d505d595783134659a959fb1760b
Niall Howard <14473278@students.lincoln.ac.uk>
2020-06-09 14:00:21 +0100

    *Hello-world starting point

--- js/js.js
+++ js/js.js
@@ -1,80 +1,23 @@
 $(document).ready(function(){
 
-  const $slider = $(".pages");
-    $slider.on('init', () => {
-        mouseWheel($slider)
-    })
-    .slick({
-        dots: true,
-        vertical: true,
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        speed: 1000,
-        autoplay: false,
-        verticalSwiping: false,
 
-    });
 
-    $('a[data-slide]').click(function(e) {
-       // e.preventDefault();
-       var slideno = $(this).data('slide');
-       $('.pages').slick('slickGoTo', slideno - 1);
-    });
-
-    $(".box-container .box ").click(function(){
-        $('.box-container .box').not(this).removeClass('selected');
-        $(this).toggleClass('selected');
-
-       if ( $('.box.previewlegacy').hasClass("selected") ) {
-            $( ".description.previewlegacy" ).show();
-            $( ".description.mirror" ).hide();
-            $( ".description.boozer" ).hide();
-            $( ".description.badminton" ).hide();
-       }
-        if ( $('.box.mirror').hasClass("selected") ) {
-            $( ".description.mirror" ).show();
-            $( ".description.boozer" ).hide();
-            $( ".description.badminton" ).hide();
-            $( ".description.previewlegacy" ).hide();
-
-       }
-       if ( $('.box.boozer').hasClass("selected") ) {
-            $( ".description.boozer" ).show();
-            $( ".description.mirror" ).hide();
-            $( ".description.badminton" ).hide();
-            $( ".description.previewlegacy" ).hide();
-
-       }
-       if ( $('.box.badminton').hasClass("selected") ) {
-            $( ".description.badminton" ).show();
-            $( ".description.mirror" ).hide();
-            $( ".description.boozer" ).hide();
-            $( ".description.previewlegacy" ).hide();
-
-       }
-    });
-
-    $('.work .right .description').matchHeight({ byRow: false });
 
 });
 
+var serverTime = new Date();
 
+function updateTime() {
+    /// Increment serverTime by 1 second and update the html for '#time'
+    serverTime = new Date(serverTime.getTime() + 1000);
 
-
-function mouseWheel($slider) {
-    $(window).on('wheel', { $slider: $slider }, mouseWheelHandler)
+    var mydate = serverTime.toLocaleString();
+    var result = mydate.replace(/,/g, "<br>");
+   
+    $('#time').html(result);
 }
-    
-function mouseWheelHandler(event) {
-    // event.preventDefault()
-    const $slider = event.data.$slider
-    const delta = event.originalEvent.deltaY
-    if(delta > 0) {
-        $slider.slick('slickPrev')
-    }
-    else {
-        $slider.slick('slickNext')
-    }
-}
+
+$(function() {
+    updateTime();
+    setInterval(updateTime, 1000);
+});
