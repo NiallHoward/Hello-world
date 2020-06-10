7fb81d87e4e93a644b98499f9cdd74e58794887c
Niall Howard <14473278@students.lincoln.ac.uk>
2020-06-10 14:12:46 +0100

    *Weather API

--- js/js.js
+++ js/js.js
@@ -1,8 +1,43 @@
 $(document).ready(function(){
 
+	// e22036c943b3dd91fce27981e4d23cd3
+	// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={YOUR API KEY}
+	// https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02 (samples API key)
+	// convert kelvins to celsius
 
+	fetch('http://api.openweathermap.org/data/2.5/forecast?id=2633352&APPID=e22036c943b3dd91fce27981e4d23cd3')
+		.then(response => response.json())
+		.then(function(data) {
+			
+			if (data.list[0].weather[0].main = "Rain"){
+				document.getElementById("tempicon").innerHTML = "<i class=\"fas fa-cloud-showers-heavy\"></i>";
+			} 
+			else if( data.list[0].weather[0].main = "Drizzle"){
+				document.getElementById("tempicon").innerHTML = "<i class=\"fas fa-cloud-rain\"></i>";
+			} 
+			else if (data.list[0].weather[0].main = "Thunderstorm" ){
+				document.getElementById("tempicon").innerHTML = "<i class=\"fas fa-bolt\"></i>";
+			}
+			else if (data.list[0].weather[0].main = "Snow" ){
+				document.getElementById("tempicon").innerHTML = "<i class=\"fas fa-snowflake\"></i>";
+			}
+			else if (data.list[0].weather[0].main = "Clear" ){
+				document.getElementById("tempicon").innerHTML = "<i class=\"fas fa-sun\"></i>";
+			}
+			else if (data.list[0].weather[0].main = "Clouds" ){
+				document.getElementById("tempicon").innerHTML = "<i class=\"fas fa-cloud\"></i>";
+			}
+			else {
 
+			}
 
+			var desc = data.list[0].weather[0].description;
+			var cel = data.list[0].main.temp;
+			var celround = cel - 273.15;
+  			
+  			document.getElementById("temp").innerHTML = desc + " " + Math.round(celround) + " &#8451;";
+    });
+		
 });
 
 var serverTime = new Date();
